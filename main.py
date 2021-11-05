1b849dbefa3d034b9024f68230126d303dfde5a2
NOP <57258174+mplein2@users.noreply.github.com>
2021-11-05 17:30:18 +0200

    time

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ import numpy
 
 
 def clean_dataset(dataset):
-    st.header("Data Cleaning")
-    st.write("We are going to process our data to better suit our needs.")
+    st.header('Data Cleaning')
+    st.write('We are going to process our data to better suit our needs.')
     # TODO MAKE WRITE BEAUTIFUL 1
     st.write(dataset)
-    st.write("Age and platelets are integers so we are going to cast them accordingly. We are also going to set the precision to 2 decimals.")
+    st.write(
+        "Age and platelets are integers so we are going to cast them accordingly. We are also going to set the precision to 2 decimals. Also "
+        "time is not relevant with predicting the chance of heart attack so it can be dropped.")
     code = '''    dataset['age'] = dataset['age'].astype(numpy.int64)
     dataset['platelets'] = dataset['platelets'].astype(numpy.int64)
-    dataset['serum_creatinine'] = dataset['serum_creatinine'].map('{:,.2f}'.format)'''
+    dataset['serum_creatinine'] = dataset['serum_creatinine'].map('{:,.2f}'.format)
+    del(dataset["time"])
+    '''
     st.code(code, language='python')
     dataset['age'] = dataset['age'].astype(numpy.int64)
     dataset['platelets'] = dataset['platelets'].astype(numpy.int64)
     dataset['serum_creatinine'] = dataset['serum_creatinine'].map('{:,.2f}'.format)
+    del(dataset["time"])
     # Making age an int since there are some rows that age is a float
     st.write(dataset)
     st.write("Our dataset looks clean enough so that it can be processed.")
     return dataset
-
 
 
 def main():
